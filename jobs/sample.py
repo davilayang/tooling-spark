1b6777bc7824ba8faf69b3baf324af2799e4ef56
davilayang <davilayang@gmail.com>
2021-06-06 22:00:54 +0100

    Add jobs sub-directory & submit with --archives for Python packages

--- jobs/sample.py
+++ jobs/sample.py
@@ -6,13 +6,19 @@ def main(spark):
     df = spark.createDataFrame(
         [(1, 1.0), (1, 2.0), (2, 3.0), (2, 5.0), (2, 10.0)],
         ("id", "v"))
+    
+    result = df.toPandas()
 
-    @pandas_udf("double")
-    def mean_udf(v: pd.Series) -> float:
-        return v.mean()
+    print(result)
+   #  @pandas_udf("double")
+   #  def mean_udf(v: pd.Series) -> float:
+   #      return v.mean()
 
-    print(df.groupby("id").agg(mean_udf(df['v'])).collect())
+   #  print(df.groupby("id").agg(mean_udf(df['v'])).collect())
 
 
 if __name__ == "__main__":
     main(SparkSession.builder.getOrCreate())
+
+# submit a job from jupyter server instance
+# spark-submit --master spark://spark-master:7077 jobs/sample.py
